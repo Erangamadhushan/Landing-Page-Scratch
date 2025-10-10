34468538d55b9be30f63dc5d65859489ae54816e
Erangamadhushan <erangamadhushann956@gmail.com>
2025-10-10 21:36:01 +0530

    feat: add API service for user management with CRUD operations

--- src/pages/User/Projects.jsx
+++ src/pages/User/Projects.jsx
@@ -1,25 +1,11 @@
 import React from 'react'
-import { useContext } from 'react';
-import { UserContext } from '../../context/UserContext';
-import { Navigate, useLocation } from 'react-router-dom';
 
 export const Projects = () => {
-    const { user } = useContext(UserContext);
-    // to get previous location: const location = useLocation();
-    const location = useLocation();
-
-    console.log('Current location:', location);
-
-    if (!user) {
-        console.log('User not found, redirecting to sign-in');
-        localStorage.setItem('redirectAfterLogin', location.pathname);
-        return <Navigate to="/signin" />;
-    }
-
-    return (
-        <div className="text-center text-yellow-400">
-            <h1 className="text-4xl font-bold mb-6">Your Projects</h1>
-            <p className="text-lg">This is where your projects will be displayed.</p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col ">
+      <h1 className="text-3xl font-bold mb-4">Projects Page</h1>
+      <p className="text-lg">This is the Projects page.</p>
+      
+    </div>
+  )
 }
